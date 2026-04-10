45b13fa2fedd78ca08afe2378684eef7214ae1e1
ThallesSorrilha <thallessorrilha@gmail.com>
2026-04-10 17:35:22 -0300

    feature(mecanica/models): adicionando classes do projeto

---
+++ mecanica/models.py
@@ -0,0 +1,99 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=60)
+    cpf = models.CharField(unique=True, max_length=11)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nome} - {self.data_nascimento} - {self.cpf}"
+
+
+class Carro(models.Model):
+    vin = models.CharField(max_length=17)
+    placa = models.CharField(max_length=7)
+    modelo = models.CharField(max_length=80)
+    cor = models.CharField(max_length=20)
+    ano = models.IntegerField()
+    quilometragem = models.PositiveIntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.modelo} - {self.cor} - {self.placa}"
+
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=60)
+    descricao = models.CharField(max_length=200, null=True)
+    preco = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return f"{self.nome}"
+
+
+class Peca(models.Model):
+    nome = models.CharField(max_length=40)
+    fabricante = models.CharField(max_length=30)
+    codigo = models.CharField(max_length=60)
+    descricao = models.CharField(max_length=100, null=True)
+    preco = models.DecimalField(decimal_places=2)
+    estoque = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nome} - {self.fabricante} - {self.codigo}"
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=60)
+    cpf = models.CharField(unique=True, max_length=11)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=20)
+    cargo = models.CharField(max_length=40)
+    salario = models.DecimalField(decimal_places=2)
+    horas_semanais = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nome} - {self.cargo} - {self.data_nascimento}"
+
+
+class Mecanico(Funcionario):
+    especialidade = models.CharField(max_length=40, null=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.especialidade}"
+
+
+class ConsultorTecnico(Funcionario):
+    def __str__(self):
+        return f"{self.nome}"
+
+
+class OrdemServico(models.Model):
+    estado = models.CharField(max_length=20)
+    preco = models.DecimalField(decimal_places=2)
+    data_inicio = models.DateTimeField(auto_now_add=True)
+    previsao_termino = models.DateTimeField(null=True)
+    data_termino = models.DateTimeField(null=True)
+    descricao = models.CharField(max_length=100, null=True)
+    observacao = models.CharField(max_length=200, null=True)
+    carro = models.ForeignKey(Carro, on_delete=models.PROTECT)
+    consultorTecnico = models.ForeignKey(
+        ConsultorTecnico, on_delete=models.PROTECT)
+    mecanicos = models.ManyToManyField(Mecanico)
+    servicos = models.ManyToManyField(Servico)
+
+    def __str__(self):
+        return f"{self.carro.modelo} - {self.carro.cor} - {self.estado} - {self.data_inicio}"
+
+
+class ItensPeca(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.PROTECT)
+    ordem_servico = models.ForeignKey(OrdemServico, on_delete=models.PROTECT)
+    quantidade = models.PositiveIntegerField()
+    preco = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return f"({self.peca.nome} - {self.ordem_servico.carro.vin} - {self.quantidade})"
